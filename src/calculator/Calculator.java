edb1335affd3f5b9711a667c7de117da890f16b3
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-09-09 02:18:41 -0300

    feat(Calculator): parse, belongsTo, operate

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -7,25 +7,22 @@ public class Calculator {
     private static String operators[] = {"+", "-", "*", "/"};
     private static Stack<Integer> stack = new Stack<>(200);
 
-    public static void parse(String expression) {
+    public static Stack<Integer> parse(String expression) {
         String sentences[] = expression.trim().split("\\s");
-
-        Integer value1;
-        Integer value2;
         String operator;
-        
 
         for(int i = 0; i < sentences.length; i++){
 
             if(belongsTo(sentences[i])){
                 operator = sentences[i];
-                
+                operate(operator);
             } else {
                 stack.push(Integer.parseInt(sentences[i]));
             }
 
         }
 
+        return stack;
     }
 
     private static boolean belongsTo(String ch){
@@ -33,6 +30,33 @@ public class Calculator {
             if(ch.equals(op)) { return true; }
         }
         return false;
+    }
+
+    private static void operate(String operator){
+        Integer last = stack.peek();
+        Integer penult = stack.penult();
+        Integer result = null;
+
+        switch (operator) {
+            case "+":
+                result = penult + last;
+                break;
+            case "-":
+                result = penult - last;
+                break;
+            
+            case "*":
+                result = penult * last;
+                break;
+
+            case "/":
+                result = penult / last;
+                break;
+        }
+
+        stack.pop();
+        stack.pop();
+        stack.push(result);
     }
     
 }
