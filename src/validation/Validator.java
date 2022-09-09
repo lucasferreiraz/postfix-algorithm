8dd0ed682badc77850b6eeb0d40ccf3fc93075f4
aluno <aluno@BL3-INFO02-18.maracanau.ifce.edu.br>
2022-09-09 08:58:21 -0300

    feat(Validator): intialOperands, notNumeric

--- src/validation/Validator.java
+++ src/validation/Validator.java
@@ -1,5 +1,31 @@
 package validation;
 
 public class Validator {
+
+    private static String operators[] = {"+", "-", "*", "/"};
     
+    public static void initialOperands(String expression){
+        String sentences[] = expression.trim().split("\\s");
+
+        for(int i = 0; i < 2; i++){
+            if(isNumeric(sentences[i])){
+            } else {
+                throw new NumberFormatException("Os dois primeiros elementos precisam ser numeros.");
+            }
+        }
+
+    }
+
+    private static boolean isNumeric(String ch){
+        if (ch == null){ return false; }
+
+        try {
+            Integer num = Integer.parseInt(ch);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
