1edd9be920d765bcf0355390020bc319990f4c34
Lucas <ferreiralucas090@gmail.com>
2022-09-09 09:10:25 -0300

    refactor(Validator): intialOperands, notNumeric

--- src/validation/Validator.java
+++ src/validation/Validator.java
@@ -4,28 +4,32 @@ public class Validator {
 
     private static String operators[] = {"+", "-", "*", "/"};
     
-    public static void initialOperands(String expression){
+    public static void validate(String expression){
         String sentences[] = expression.trim().split("\\s");
 
+        initialOperands(sentences);
+    }
+
+    private static void initialOperands(String[] sentences){
+
         for(int i = 0; i < 2; i++){
-            if(isNumeric(sentences[i])){
-            } else {
+            if(notNumeric(sentences[i])){
                 throw new NumberFormatException("Os dois primeiros elementos precisam ser numeros.");
             }
         }
 
     }
 
-    private static boolean isNumeric(String ch){
-        if (ch == null){ return false; }
+    private static boolean notNumeric(String ch){
+        if (ch == null){ return true; }
 
         try {
             Integer num = Integer.parseInt(ch);
         } catch (NumberFormatException e) {
-            return false;
+            return true;
         }
 
-        return true;
+        return false;
     }
 
 }
