38e1e2eaaf3b4cec8a42656d77ad28865bbb94d6
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-09-09 03:23:08 -0300

    wip: main App class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
+import calculator.Calculator;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        //3 4 +
+        System.out.println("Type the expression in postfix format: ");
+        String expression = input.nextLine();
+
+        System.out.println(Calculator.parse(expression)); 
+
+        input.close();
     }
 }
