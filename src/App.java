82b64b80f77805c374986fa209181b4db4535a1e
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-09-10 17:27:05 -0300

    feat(Validator): lastElement, validElements

--- src/App.java
+++ src/App.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
 import calculator.Calculator;
+import validation.Validator;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        //3 4 +
+
         System.out.println("Type the expression in postfix format: ");
         String expression = input.nextLine();
 
-        System.out.println(Calculator.parse(expression)); 
-
+        Validator.validate(expression);
+        System.out.println("The result is: " + Calculator.parse(expression)); 
+        
         input.close();
     }
 }
--- src/validation/Validator.java
+++ src/validation/Validator.java
@@ -1,34 +1,53 @@
 package validation;
 
 public class Validator {
-
-    private static String operators[] = {"+", "-", "*", "/"};
     
     public static void validate(String expression){
         String sentences[] = expression.trim().split("\\s");
-
+        
         initialOperands(sentences);
+        lastElement(sentences);
+        validElements(sentences);
     }
 
     private static void initialOperands(String[] sentences){
-
         for(int i = 0; i < 2; i++){
             if(notNumeric(sentences[i])){
                 throw new NumberFormatException("Os dois primeiros elementos precisam ser numeros.");
             }
         }
+    }
 
+    private static void lastElement(String[] sentences){
+        int size = sentences.length;
+        if(!notNumeric(sentences[size-1])){
+            throw new IllegalArgumentException("The last element must be an operator.");
+        }
+    }
+
+    public static void validElements(String[] sentences){
+        int numbers = 0;
+        int operators = 0;
+        for(String element : sentences){
+            if(notNumeric(element)){
+                operators++;
+            } else if(!notNumeric(element)){
+                numbers++;
+            }
+        }
+
+        if(operators != numbers-1){
+            throw new RuntimeException("Invalid amount of numbers and/or operators");
+        }
     }
 
     private static boolean notNumeric(String ch){
         if (ch == null){ return true; }
-
         try {
-            Integer num = Integer.parseInt(ch);
+            Integer.parseInt(ch);
         } catch (NumberFormatException e) {
             return true;
         }
-
         return false;
     }
 
